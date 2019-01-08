a7b03d2ed42fb3bcff00cc12de5517853b9966a4
Light-Moon <1424686850@qq.com>
2019-01-08 17:16:23 +0800

    Added xmlUtils.py and updated master.py

--- DFS_TEST/package/scripts/master.py
+++ DFS_TEST/package/scripts/master.py
@@ -6,8 +6,10 @@ from resource_management.core.exceptions import ComponentIsNotRunning
 from resource_management.core.environment import Environment
 from resource_management.core.logger import Logger
 from resource_management.libraries.script.script import Script
+from xmlUtils import write_xml
 
 class Master(Script):
+    dfs_conf_dir = '/apps/dfs'
     def install(self, env):
         print 'Install My Master';
     def stop(self, env):
@@ -20,12 +22,35 @@ class Master(Script):
         status,output = commands.getstatusoutput("ls ~")
         print 'status code: ', status
         print 'output: ', output
-        print "Start My Master"
         config = Script.get_config()
-		kv_dict = config['configurations']['dfs-site']
-		commands.getstatus('rm ./../../configuration/zql1.xml')
-		output_path = './../../configuration/zql.xml'
-		write_xml(kv_dict, output_path)
+        script_path = os.getcwd()
+        conf_dir = script_path + '/../../configuration'
+        if not os.path.isdir(conf_dir):
+           # os.mkdir(conf_dir)   
+            Directory([conf_dir],mode=0755,owner='dfs',group='dfs',create_parents=True) 
+        filenames = ['dfs-site.xml']
+        for filename in filenames:
+            file_path = conf_dir + '/' + filename
+            if not os.path.isfile(file_path):
+               # f = open(file_path, 'w')
+               # f.close()
+                File([file_path],mode=0755,owner='dfs',group='dfs')
+            prefix_filename = filename[:-4]
+            dict = config['configurations'][prefix_filename]
+            write_xml(dict, file_path)
+            
+            dfs_file_path = dfs_conf_dir + '/' + filename
+            if not os.path.isfile(dfs_file_path):
+                status,output = commands.getstatusoutput("ln -s file_path dfs_file_path")
+                print "execute 'ln' status code: ", status
+       # status,output = commands.getstatusoutput("pwd")
+       # print 'status code: ', status
+       # print 'output: ', output
+       # commands.getstatus('rm ./../../configuration/zql1.xml')
+       # output_path = './../../configuration/zql.xml'
+       # output_path = output + '/zql.xml'
+       # File([output_path],mode=0755,owner='dfs',group='dfs',content='zzzzz')
+       # write_xml(dict, output_path)
        # Logger.info(config['configurations']['dfs-site']['dfs.supergroup'])
        # Logger.info(config['configurations']['dfs-site'])
         print 'configuration of dfs.supergroup:', config['configurations']['dfs-site']['dfs.supergroup']
@@ -35,6 +60,7 @@ class Master(Script):
        # print 'tmp: ', tmp
        # File(['/apps/dfs/zql.xml'],mode=0755,owner='dfs',group='dfs',content='zzzzz')
        # Directory(['/apps/dfs/zql/dir1'],mode=0755,owner='dfs',group='dfs',create_parents=True)
+        print "Start My Master"
     def status(self, env):
         pid =  format ("/apps/dfs/dfs.pid")
         check_process_status(pid)
@@ -43,3 +69,4 @@ class Master(Script):
         print 'Configure the Sample Srv Master';
 if __name__ == "__main__":
     Master().execute()
+   # Master().execute().start(self,env)
--- DFS_TEST/package/scripts/slave.py
+++ DFS_TEST/package/scripts/slave.py
@@ -1,15 +1,29 @@
-import sys
+import sys,os
+import commands
 from resource_management import *
+from resource_management.core.exceptions import ComponentIsNotRunning
+from resource_management.core.environment import Environment
+from resource_management.core.logger import Logger
+
 class Slave(Script):
-  def install(self, env):
-    print 'Install the Sample Srv Slave';
-  def stop(self, env):
-    print 'Stop the Sample Srv Slave';
-  def start(self, env):
-    print 'Start the Sample Srv Slave';
-  def status(self, env):
-    print 'Status of the Sample Srv Slave';
-  def configure(self, env):
-    print 'Configure the Sample Srv Slave';
+    def install(self, env):
+        print 'Install the Sample Srv Slave';
+    def stop(self, env):
+        status,output = commands.getstatusoutput("ls /apps/dfs")
+        print 'status code: ', status
+        print 'output: ', output
+        Execute('rm -f /apps/dfs/dfs_slave.pid')
+        print "Stop My Slave" 
+    def start(self, env):
+        status,output = commands.getstatusoutput("ls")
+        print 'status code: ', status
+        print 'output: ', output
+        print "Start My Slave" 
+    def status(self, env):
+        pid =  format ("/apps/dfs/dfs_slave.pid")
+        check_process_status(pid)
+        print 'Status of the My Slave';  
+    def configure(self, env):
+        print 'Configure the Sample Srv Slave';
 if __name__ == "__main__":
-  Slave().execute()
+    Slave().execute()
