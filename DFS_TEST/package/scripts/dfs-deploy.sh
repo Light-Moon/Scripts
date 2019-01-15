17a1f90ac1145835e1f31032544f1c4a94d33e56
Light-Moon <1424686850@qq.com>
2019-01-15 18:40:20 +0800

    Added configuration parameters and usage explain for shell script.

--- DFS_TEST/package/scripts/dfs-deploy.sh
+++ DFS_TEST/package/scripts/dfs-deploy.sh
@@ -1,48 +1,94 @@
 #!/bin/bash
-user=autodfs
-group=autodfs
+#首先要把ctdfs的安装包手动上传到hdfs指定目录上并利用hadoop fs -chown -R dfs:dfs /apps更改所属用户及组
+#Default parameters value.
+superuser=autodfs
+supergroup=autodfs
 keytab_path=/etc/security/keytabs
-user_root_path=/home/${user}
+user_root_path=/home/${superuser}
 component_name=ctdfs
-#TODO:首先要把ctdfs的安装包手动上传到hdfs指定目录上并利用hadoop fs -chown -R dfs:dfs /apps更改所属用户及组
-default_package_path=/apps/ctdfs.tar.gz
-default_compressed_package_name=ctdfs.tar.gz
+install_package_path=/apps/ctdfs.tar.gz
+compressed_package_name=ctdfs.tar.gz
+
+#If there is no args to specified, it will show usage.
+if [ $# = 0 ]; then
+	echo "Usage: $0 [-u <username>] [-g <groupname>] [-p <install_package_path>]"
+	echo "Try '$0 --help' for more information."
+fi
+
+if [[ $# = 1 ]] && [[ $1 = '--help' ]]; then
+	echo "*******************************start***********************************"
+	echo "The number of parameters can be zero or more!Default values of parameters are:"
+	echo "username : [${superuser}]"
+	echo "groupname : [${supergroup}]"
+	echo "install_package_path : [${install_package_path}]"
+	echo "$0 [-u <username>]  -->  set only username , others take default values."
+	echo "$0 [-u <groupname>]  -->  set only groupname , others take default values."
+	echo "$0 [-u <install_package_path>]  -->  set only install_package_path , others take default values."
+	echo "$0 [-u <username>] [-u <groupname>]  -->  set two parameters , install_package_path take default values."
+	echo "and so on ..."
+	echo "********************************end**********************************"
+fi
+
+#Get arguments.
+while getopts ":u:g:p:" opt
+do
+	case ${opt} in
+		u)
+			superuser=$OPTARG
+			echo "superuser = [$superuser]"
+			;;
+        g)
+			supergroup=$OPTARG
+			echo "supergroup = [$supergroup]"
+			;;
+        p)
+			install_package_path=$OPTARG
+			echo "install_package_path = [$install_package_path]"
+			;;
+        ?)
+			echo "Unknown parameter error!"
+			echo "Usage: $0 [-u <username>] [-g <groupname>] [-p <install_package_path>]"
+			exit 1
+			;;
+    esac
+done
+
 
 ########################################
-#Step1: Configure group for CTDFS in host
+#Step1: Configure supergroup for CTDFS in host
 
-egrep "^${group}:" /etc/group > /dev/null 2>&1
+egrep "^${supergroup}:" /etc/group > /dev/null 2>&1
 if [ $? -ne 0 ]
 then
-	echo "Group of [${group}] is not exist! Now to create ..."
-    groupadd $group
+	echo "Group of [${supergroup}] is not exist! Now to create ..."
+    groupadd $supergroup
 	if [ $? -ne 0 ]
 	then
-		echo "Create group of [${group}] success!"
+		echo "Create group of [${supergroup}] success!"
 	fi
 else
-	echo "Group of ${group} is existing! No need to create!"
+	echo "Group of ${supergroup} is existing! No need to create!"
 fi
 
 ########################################
-#Step2: Configure user for CTDFS in host
+#Step2: Configure superuser for CTDFS in host
 
-user_infos=`cat /etc/passwd|grep ^${user}:`
-if [ -z "${user_infos}" ]; then
-    echo "User of ${user} is not exist! Now to add user ..."
-	useradd -g ${group} -d ${user_root_path} -m ${user}
+superuser_infos=`cat /etc/passwd|grep ^${superuser}:`
+if [ -z "${superuser_infos}" ]; then
+    echo "User of ${superuser} is not exist! Now to add user ..."
+	useradd -g ${supergroup} -d ${user_root_path} -m ${superuser}
 	if [ $? -ne 0 ]
 	then
-		echo "Add user of [${user}] success!"
+		echo "Add user of [${superuser}] success!"
 	fi
-	# read -p "please input password for ${user} user:" password	
-	# echo ${password} |passwd --stdin ${user}	
+	# read -p "please input password for ${superuser} user:" password	
+	# echo ${password} |passwd --stdin ${superuser}	
 else
-    echo "User of ${user} is existing! No need to create!"
-	#TODO: add ${user} to ${group}
-    substr=${user_infos##*::}
+    echo "User of ${superuser} is existing! No need to create!"
+	#TODO: add ${superuser} to ${supergroup}
+    substr=${superuser_infos##*::}
     user_root_path=${substr%%:*}
-    echo "The root path of ${user} user is :[${user_root_path}]"
+    echo "The root path of ${superuser} user is :[${user_root_path}]"
 fi
 
 ########################################
@@ -50,25 +96,25 @@ fi
 
 kerberos_username=root/admin
 kerberos_password=root
-keytab_name=${user}.service.keytab
+keytab_name=${superuser}.service.keytab
 domain_name=`hostname -f`
 echo "The domain_name is : [${domain_name}]"
-#`/usr/bin/kadmin -p ${kerberos_username} -w ${kerberos_password} -q 'ank -randkey ${user}/${domain_name}'`
-#`/usr/bin/kadmin -p ${kerberos_username} -w ${kerberos_password} -q 'xst -k ${keytab_path}/${keytab_name} ${user}/${domain_name}'`
-#echo "0 0 * * * /usr/bin/kinit -k -t ${keytab_path}/${keytab_name} ${user}/${domain_name}" >> /var/spool/cron/${user}
-#`crontab /var/spool/cron/${user}`
-#`chown ${user}:${user} /var/spool/cron/${user}`&&`chmod 644 /var/spool/cron/${user}`
+#`/usr/bin/kadmin -p ${kerberos_username} -w ${kerberos_password} -q 'ank -randkey ${superuser}/${domain_name}'`
+#`/usr/bin/kadmin -p ${kerberos_username} -w ${kerberos_password} -q 'xst -k ${keytab_path}/${keytab_name} ${superuser}/${domain_name}'`
+#echo "0 0 * * * /usr/bin/kinit -k -t ${keytab_path}/${keytab_name} ${superuser}/${domain_name}" >> /var/spool/cron/${superuser}
+#`crontab /var/spool/cron/${superuser}`
+#`chown ${superuser}:${supergroup} /var/spool/cron/${superuser}`&&`chmod 644 /var/spool/cron/${superuser}`
 #`chmod 644 ${keytab_path}/${keytab_name}`
 echo "Current user is : [`whoami`]"
-#`su - ${user} -c "/usr/bin/kinit -k -t ${keytab_path}/${keytab_name} ${user}/${domain_name}"`
+#`su - ${superuser} -c "/usr/bin/kinit -k -t ${keytab_path}/${keytab_name} ${superuser}/${domain_name}"`
 
 ########################################
 #Step4: Read script parameters.
 #	param1:Installation pack storage path in HDFS.
 
 if [ -n "$1" ]; then
-	${default_package_path}=$1
-	${default_compressed_package_name}=${1##*/}
+	${install_package_path}=$1
+	${compressed_package_name}=${1##*/}
 fi
 
 ########################################
@@ -80,19 +126,19 @@ if [ -d "${user_root_path}/${component_name}" ]; then
 		echo "Backup folder of ${component_name} success!"
 	fi
 fi
-`su - ${user} -c "mkdir ${user_root_path}/${component_name}"`
-if [ -f "${user_root_path}/${default_compressed_package_name}" ]; then
+`su - ${superuser} -c "mkdir ${user_root_path}/${component_name}"`
+if [ -f "${user_root_path}/${compressed_package_name}" ]; then
 	echo "Install package is existed , no need to download!"	
 else
-	`su - ${user} -c "hadoop fs -get ${default_package_path} ${user_root_path}"`
+	`su - ${superuser} -c "hadoop fs -get ${install_package_path} ${user_root_path}"`
 	if [ $? -eq 0 ]; then
 		echo "Download install package success!"	
 	else
 		echo "Download install package fail and exit shell script execution now!"
-		exit
+		exit 1
 	fi
 fi
-`su - ${user} -c "tar -xvzf ${user_root_path}/${default_compressed_package_name} -C ${user_root_path}/${component_name} --strip-components 1 > /dev/null "`
+`su - ${superuser} -c "tar -xvzf ${user_root_path}/${compressed_package_name} -C ${user_root_path}/${component_name} --strip-components 1 > /dev/null "`
 if [ $? -eq 0 ]; then
 	echo "Decompression install package success!"
 else
@@ -129,8 +175,10 @@ fi
 ########################################
 #Step8: Create soft links for configuration files
 
-#dfs自身的配置文件并未读配置并更新写入文件，在master的启动模块统一更新
-dfs_conf_dir=/var/lib/ambari-agent/cache/stacks/HDP/2.5/services/DFS_TEST/configuration
+# dfs自身的配置文件并未读配置并更新写入文件，在master的启动模块统一更新
+#dfs_conf_dir=/var/lib/ambari-agent/cache/stacks/HDP/2.5/services/DFS_TEST/configuration
+shell_script_path=`dirname "$0"`
+dfs_conf_dir=`cd ${shell_script_path}/../../configuration; pwd`
 hdfs_conf_dir=/etc/hadoop/conf
 hbase_conf_dir=/etc/hbase/conf
 conf_dir=${user_root_path}/${component_name}/conf
@@ -154,7 +202,7 @@ done
 
 ########################################
 #Step9: Initialize CTDFS Component 
-`su - ${user} -c "${user_root_path}/${component_name}/bin/dfsadmin -init ${keytab_path}/${keytab_name}"`
+`su - ${superuser} -c "${user_root_path}/${component_name}/bin/dfsadmin -init ${keytab_path}/${keytab_name}"`
 if [ $? -eq 0 ]
 then
 	echo "Component initialization success!"
