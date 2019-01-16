b0c4f713e8be0ed7ec4e4d303c81a9dc50f9c353
Light-Moon <1424686850@qq.com>
2019-01-16 17:40:05 +0800

    Added create-softlinks.sh

---
+++ DFS_TEST/package/scripts/create-softlinks.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#If there is no args to specified, it will show usage.
+if [ $# = 0 ]; then
+	echo "Usage: $0 <user_root_path> <component_folder_name>"
+	echo "Try '$0 --help' for more information."
+elif [[ $# = 1 ]] && [[ $1 = '--help' ]]; then
+	echo "*******************************start*********************************"
+	echo "The shell script needs 2 parameters separated by blank space(s)."
+	echo "For Example: sh $0 /home/dfs ctdfs"
+	echo "********************************end**********************************"
+fi
+if [[ $1 = */ ]];then
+	$1=${1%*/}
+fi
+#dfs_conf_dir=/var/lib/ambari-agent/cache/stacks/HDP/2.5/services/DFS_TEST/configuration
+shell_script_path=`dirname "$0"`
+dfs_conf_dir=`cd ${shell_script_path}/../../configuration; pwd`
+hdfs_conf_dir=/etc/hadoop/conf
+hbase_conf_dir=/etc/hbase/conf
+conf_dir=$1/$2/conf
+hdfs_conf_files=("core-site.xml" "hdfs-site.xml" "mapred-site.xml" "yarn-site.xml" "log4j.properties")
+hbase_conf_file=hbase-site.xml
+for hdfs_conf_file in "${hdfs_conf_files[@]}"
+do
+	`ln -s ${hdfs_conf_dir}/${hdfs_conf_file} ${conf_dir}/${hdfs_conf_file}`
+done
+`ln -s ${hbase_conf_dir}/${hbase_conf_file} ${conf_dir}/${hbase_conf_file}`
+`ln -s ${hbase_conf_dir}/${hbase_conf_file} ${conf_dir}/dfs-${hbase_conf_file}`
+for dfs_conf_file in `find ${dfs_conf_dir} -maxdepth 1 -name '*.xml' -type f`
+do
+	file_name=${dfs_conf_file##*/}
+	echo "dfs_conf_file is : [${file_name}]"
+	#if [ "${file_name}" == "dfs-site.xml" ]; then
+	#	`ln -s ${dfs_conf_file} ${conf_dir}/dfs-default.xml`
+	#fi
+	`ln -s ${dfs_conf_file} ${conf_dir}/${file_name}`
+done
