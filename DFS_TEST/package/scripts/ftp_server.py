8233c09ed9385a7a9ef51d9c1de4c028bd787f98
Light-Moon <1424686850@qq.com>
2019-01-16 15:53:06 +0800

    A simple ftp_server.py

--- DFS_TEST/package/scripts/ftp_server.py
+++ DFS_TEST/package/scripts/ftp_server.py
@@ -1,9 +1,29 @@
-import sys
+import sys,os
+import commands
 from resource_management import *
-class SampleClient(Script):
+from resource_management.core.exceptions import ComponentIsNotRunning
+from resource_management.core.environment import Environment
+from resource_management.core.logger import Logger
+
+class Slave(Script):
     def install(self, env):
-        print 'Install the Sample Srv Client';
+        print 'Install the Ftp Server';
+    def stop(self, env):
+        print "Stop Ftp Server"
+        status,output = commands.getstatusoutput("ls /apps/dfs")
+        print 'status code: ', status
+        print 'output: ', output
+        Execute('rm -f /apps/dfs/dfs_slave.pid')
+    def start(self, env):
+        print "Start Ftp Server"
+        status,output = commands.getstatusoutput("ls")
+        print 'status code: ', status
+        print 'output: ', output
+    def status(self, env):
+        print "Status Ftp Server"
+        pid =  format ("/apps/dfs/dfs_slave.pid")
+        check_process_status(pid)
     def configure(self, env):
-        print 'Configure the Sample Srv Client';
+        print "Configure Ftp Server"
 if __name__ == "__main__":
-    SampleClient().execute()
+    Slave().execute()
