aeb84058e8f8958422e626dfd735b884a12b6355
Light-Moon <1424686850@qq.com>
2019-03-06 22:28:27 +0800

    更新kerberos.py脚本中函数内容

--- CTDFS/package/scripts/kerberos.py
+++ CTDFS/package/scripts/kerberos.py
@@ -3,9 +3,35 @@ import sys,os
 import commands
 from resource_management import *
 from resource_management.libraries.script.script import Script
+import params
 
+config = Script.get_config()
+
+def getKerberosStatus():
+    kerberos_status = config['configurations']['ctdfs-kerberos-site']['dfs.kerberos.enabled']
+	return kerberos_status
+
+def getMergeKeytabsHost():
+    merge_keytabs_host = config['configurations']['ctdfs-kerberos-site']['dfs.merge.keytab.host']
+	return merge_keytabs_host
+	
+def getKerberosPrincipal():
+    kerberos_principal = config['configurations']['ctdfs-kerberos-site']['kadmin.local.principal']
+	return kerberos_principal
+	
+def getKerberosPassword():
+    kerberos_principal = config['configurations']['ctdfs-kerberos-site']['kadmin.local.password']
+	return kerberos_password
+	
+def getKeytabPath():
+    keytab_path = params.keytab_path
+    return keytab_path
+	
+def getKeytabName():
+    keytab_name = params.keytab_name
+    return keytab_name
+	
 def getKerberosInfo(param):
-    config = Script.get_config()
 	if param == 1:
         kerberos_principal = config['configurations']['ctdfs-kerberos-site']['kadmin.local.principal']
         #kerberos_principal=111
@@ -16,6 +42,3 @@ def getKerberosInfo(param):
 	    #kerberos_password = 222
 		#value2=`python -c 'import kerberos; print kerberos.getKerberosInfo(2)'`
 	    return kerberos_password
-	
-if __name__ == "__main__":
-    getKerberosInfo()
