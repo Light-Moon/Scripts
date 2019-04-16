18c657e7128afabdbb3a4612f08e8a7e28dbf8ec
Light-Moon <1424686850@qq.com>
2019-04-16 17:36:15 +0800

    Delete useless comment statements and change log form for master.py

--- CTDFS/package/scripts/master.py
+++ CTDFS/package/scripts/master.py
@@ -10,32 +10,31 @@ from resource_management.libraries.script.script import Script
 from xml_utils import write_xml
 
 class Master(Script):
-    MASTER_STATE = True
     def install(self, env):
         import params
         env.set_params(params)
         import kerberos
-        print '********** Install CTDFS_MASTER Operation Begin **********';
+        Logger.info("********** Install CTDFS_MASTER Operation Begin **********")
         print "Current superuser is [" + params.superuser + "]; Current supergroup is [" + params.supergroup + "]; Current install_package_path is [" + params.install_package_path + "]; kerberos status is [" + kerberos.getKerberosStatus() + "]; merge_keytabs_host is [" + kerberos.getMergeKeytabsHost() + "]; kerberos_principal is [" + kerberos.getKerberosPrincipal() + "]; kerberos_password is [" + kerberos.getKerberosPassword() + "]."
-        status,output = commands.getstatusoutput("sh " + params.scripts_path + "/auto-deploy.sh -u " + params.superuser + " -g " + params.supergroup + " -p " + params.install_package_path + " -f " + kerberos.getKerberosStatus() + " -h " + kerberos.getMergeKeytabsHost() + " -i " + kerberos.getKerberosPrincipal() + " -k " + kerberos.getKerberosPassword() + " >> " + params.scripts_path + "/deploy.log 2>&1 ")
-        print 'Execute auto-deploy.sh status code: ', status
-        print 'Execute auto-deploy.sh output: ', output
-        if status > 0:
-            raise Exception("***********Execute auto-deploy.sh error!!!********", status)
+        auto_deploy_status,auto_deploy_output = commands.getstatusoutput("sh " + params.scripts_path + "/auto-deploy.sh -u " + params.superuser + " -g " + params.supergroup + " -p " + params.install_package_path + " -f " + kerberos.getKerberosStatus() + " -h " + kerberos.getMergeKeytabsHost() + " -i " + kerberos.getKerberosPrincipal() + " -k " + kerberos.getKerberosPassword() + " >> " + params.scripts_path + "/deploy.log 2>&1 ")
+        Logger.info("auto_deploy_status = " + str(auto_deploy_status))
+        Logger.info("auto_deploy_output = " + auto_deploy_output)
+        if auto_deploy_status > 0:
+            raise Exception("***********Execute auto-deploy.sh error!!!********", auto_deploy_status)
         if not os.path.isdir(params.ctdfs_conf_dir):
             Directory([params.ctdfs_conf_dir],mode=0755,owner=params.superuser,group=params.supergroup,create_parents=True) 
         for filename in params.ctdfs_master_conf_filenames:
             file_path = params.ctdfs_conf_dir + '/' + filename
-            print 'ctdfs_master_conf_filename = ', file_path
+            Logger.info("ctdfs_master_conf_filename = " + file_path)
             if not os.path.isfile(file_path):
                 File([file_path],mode=0755,owner=params.superuser,group=params.supergroup)
             prefix_filename = filename[:-4]
-            print 'prefix_filename = ', prefix_filename
+            Logger.info("prefix_filename = " + prefix_filename)
             dict = params.config['configurations'][prefix_filename]
             write_xml(dict, file_path)
-        status,output = commands.getstatusoutput("sh " + params.scripts_path + "/dependent-softlinks.sh " + params.ctdfs_conf_dir + " >> " + params.scripts_path + "/deploy.log 2>&1 ")				
-        print 'create-softlinks status code:', status
-        print 'create-softlinks output:', output
+        dependent_softlinks_status,dependent_softlinks_output = commands.getstatusoutput("sh " + params.scripts_path + "/dependent-softlinks.sh " + params.ctdfs_conf_dir + " >> " + params.scripts_path + "/deploy.log 2>&1 ")				
+        Logger.info("dependent_softlinks_status = " + str(dependent_softlinks_status))
+        Logger.info("dependent_softlinks_output = " + dependent_softlinks_output)
         if os.path.isdir(params.ambari_server_conf_dir):
             link_toAmbariServer_status,link_toAmbariServer_output = commands.getstatusoutput("sh " + params.scripts_path + "/ctdfs-softlinks.sh " + params.ctdfs_conf_dir + " " + params.ambari_server_conf_dir + " >> " + params.scripts_path + "/deploy.log 2>&1 ")
             Logger.info("link_toAmbariServer_status = " + str(link_toAmbariServer_status))
@@ -72,83 +71,65 @@ class Master(Script):
             if init_status > 0:
                 raise Exception("***********Execute init example keytab error!!!***********")
             Logger.info("********** Initialization CTDFS Operation By Example Keytab End **********")
-        print '********** Install CTDFS_MASTER Operation End **********';
+        Logger.info("********** Install CTDFS_MASTER Operation End **********")
     def stop(self, env):
-        print "********** Stop CTDFS_MASTER Operation Begin **********"
-        global MASTER_STATE
-        MASTER_STATE = False
-
+        Logger.info("********** Stop CTDFS_MASTER Operation Begin **********")
         scripts_path = sys.path[0]
         master_pid = scripts_path + '/../../master.pid'
         #if not os.path.isfile(master_pid):
         fp = open(master_pid,'w')
         fp.write('False')
         fp.close()
-        Logger.info("MASTER_STATE = " + str(MASTER_STATE))
-        print "********** Stop CTDFS_MASTER Operation End **********"
+        Logger.info("********** Stop CTDFS_MASTER Operation End **********")
     def start(self, env):
         import params
         env.set_params(params)
-        print "********** Start CTDFS_MASTER Operation Begin **********"
+        Logger.info("********** Start CTDFS_MASTER Operation Begin **********")
         #status,filenames = commands.getstatusoutput("find " + params.ctdfs_conf_dir + " -maxdepth 1 -name *.xml -o -name *.properties -type f")
-        status,filenames = commands.getstatusoutput("find " + params.ctdfs_conf_dir + " -maxdepth 1 -name *.xml -type f")
-        print 'find ctdfs_conf_filenames status code: ', status
-        print 'find ctdfs_conf_filenames output: ', filenames
-        for item in filenames.split('\n'):
+        find_conf_filenames_status,find_conf_filenames_output = commands.getstatusoutput("find " + params.ctdfs_conf_dir + " -maxdepth 1 -name *.xml -type f")
+        Logger.info("find_conf_filenames_status = " + str(find_conf_filenames_status))
+        Logger.info("find_conf_filenames_output = " + find_conf_filenames_output)
+        for item in find_conf_filenames_output.split('\n'):
             file_path = item
             print 'file_path=', file_path
             filename = item.split('/')[-1]
             prefix_filename = filename[:-4]
-            print 'prefix_filename = ', prefix_filename
+            Logger.info("prefix_filename = " + prefix_filename)
             dict = params.config['configurations'][prefix_filename]
             write_xml(dict, file_path)
-        status,output = commands.getstatusoutput("sh " + params.scripts_path + "/dependent-softlinks.sh " + params.ctdfs_conf_dir + " >> " + params.scripts_path + "/deploy.log 2>&1 ")
-        print 'create-softlinks status code:', status
-        print 'create-softlinks output:', output
+        dependent_softlinks_status,dependent_softlinks_output = commands.getstatusoutput("sh " + params.scripts_path + "/dependent-softlinks.sh " + params.ctdfs_conf_dir + " >> " + params.scripts_path + "/deploy.log 2>&1 ")
+        Logger.info("dependent_softlinks_status = " + str(dependent_softlinks_status))
+        Logger.info("dependent_softlinks_output = " + dependent_softlinks_output)
         if os.path.isdir(params.ambari_server_conf_dir):
             link_toAmbariServer_status,link_toAmbariServer_output = commands.getstatusoutput("sh " + params.scripts_path + "/ctdfs-softlinks.sh " + params.ctdfs_conf_dir + " " + params.ambari_server_conf_dir + " >> " + params.scripts_path + "/deploy.log 2>&1 ")
             Logger.info("link_toAmbariServer_status = " + str(link_toAmbariServer_status))
             Logger.info("link_toAmbariServer_output = " + link_toAmbariServer_output)
-        global MASTER_STATE
-        MASTER_STATE = True
-        Logger.info("MASTER_STATE = " + str(MASTER_STATE))
-
         scripts_path = sys.path[0]
         master_pid = scripts_path + '/../../master.pid'
         #if not os.path.isfile(master_pid):
         fp = open(master_pid,'w')
         fp.write('True')
         fp.close()
-        print "********** Start CTDFS_MASTER Operation End **********"
+        Logger.info("********** Start CTDFS_MASTER Operation End **********")
     def status(self, env):
-        #import params
-        #env.set_params(params)
-        print "********** Status CTDFS_MASTER Operation Begin **********"
-        #pid =  format(params.master_pid_dir)
-        #check_process_status(pid)
-        status,pid = commands.getstatusoutput("ps -ef|grep NameNode |grep -v grep | awk '{print $2}'")
-        #MASTER_STATE1 = open(target_master_pid).read()
-        #global MASTER_STATE
-        
+        Logger.info("********** Status CTDFS_MASTER Operation Begin **********")
         scripts_path = sys.path[0]
         master_pid = scripts_path + '/../../master.pid'
         MASTER_STATE = open(master_pid).read()
         if str(MASTER_STATE).lower() != 'true':
             #if open(master_pid).read() != True:
             raise ComponentIsNotRunning()
-        print "********** Status CTDFS_MASTER Operation End **********"
+        Logger.info("********** Status CTDFS_MASTER Operation End **********")
     def configure(self, env):
-        print "********** configure CTDFS_MASTER Operation Begin **********"
-        print "Do Nothing"
-        print "********** configure CTDFS_MASTER Operation End **********"
+        Logger.info("********** configure CTDFS_MASTER Operation Begin **********")
+        Logger.info("Do Nothing")
+        Logger.info("********** configure CTDFS_MASTER Operation End **********")
     def kerberizectdfs(self, env):
         import params
         env.set_params(params)
         import kerberos
-        #TODO:可将部分变量统一定义在params.py中
         domain_name=commands.getoutput("hostname -f")
         keytab_name=params.superuser + '.' + domain_name + '.keytab'
-       
         #每台机器生成keytab并进行kinit认证
         if kerberos.getKerberosStatus() == 'true':
             Logger.info("********** Regenerate keytab and Kinit host authentication and Init ctdfs component **********")
@@ -166,7 +147,6 @@ class Master(Script):
                 scpkeytab_status,scpkeytab_output = commands.getstatusoutput("scp " + params.default_keytab_path + "/" + keytab_name + " " + params.superuser + "@" + kerberos.getMergeKeytabsHost() + ":" + params.user_root_path + "/" + params.component_folder_name + "/keytab/merge")
                 Logger.info("scpkeytab_status = " + str(scpkeytab_status))
                 Logger.info("scpkeytab_output = " + scpkeytab_output)
-            
             #合并keytab 
             if kerberos.getMergeKeytabsHost() == domain_name:
                 merge_keytab_status,merge_keytab_output = commands.getstatusoutput("sh " + params.scripts_path + "/merge_keytab.sh " + params.merge_cmds_file + " " + params.merge_keytabs_path + " " + params.merge_keytab_name + " " + params.ctdfs_keytab_path)
